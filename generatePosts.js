40f66c909d2215ce732e3d25c343b2af7b7e96d4
Alisha <aychan3225@gmail.com>
2025-09-18 10:58:33 -0400

    Fix lines and spacing

--- generatePosts.js
+++ generatePosts.js
@@ -97,11 +97,7 @@ dataRows.forEach((row, index) => {
                 <br />
                 <p class="description">${row.description || ''}</p>
                 <br />
-                <hr />
-                <br />
                 <p class="datasource">${row.datasource ? `Data source: ${row.datasource}` : ''}</p>
-                <br />
-                <hr />
                 <br />
                 <p><strong>Please note that we created these figures to be as accurate as possible, but they have not been formally peer reviewed and should not be considered definitive scholarly work</strong></p>
             </div>
